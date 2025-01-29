8b7d57d4f23feeac0fab8ba6f70403ea18f43bf4
Navid Hejazi <navid.hejazi@Tensator.com>
2025-01-29 10:10:44 +0000

    Work on mini project - Primes Founder

---
+++ 014_Primes_Finder.py
@@ -0,0 +1,138 @@
+
+# Version 1
+n = 20
+
+number_range = set(range(2, n+1))
+primrs_list = []
+
+prime = number_range.pop()
+primrs_list.append(prime)
+multiples = set(range(prime*2, n+1, prime))
+number_range.difference_update(multiples)
+
+
+prime = number_range.pop()
+primrs_list.append(prime)
+multiples = set(range(prime*2, n+1, prime))
+number_range.difference_update(multiples)
+
+
+# Version 2
+
+# upprt bound 
+n = 1000
+
+# number range to be checked
+number_range = set(range(2, n+1))
+
+# empty list to append discovered prime to
+primes_list = []
+
+# While loop
+while number_range:
+        prime = number_range.pop()
+        primes_list.append(prime)
+        multiples = set(range(prime*2, n+1, prime))
+        number_range.difference_update(multiples)
+
+# print our list of primes        
+print(primes_list)
+
+# number of primes that were found
+prime_count = len(primes_list)
+
+# largest_prime 
+largest_prime = max(primes_list)
+
+# summary
+print(f"There are {prime_count} prime numbers between 2 and {n}, the largest of which is {largest_prime} ")
+
+
+
+# Version 3
+
+
+def primes_founder(n):
+    # number range to be checked
+    number_range = set(range(2, n+1))
+
+    # empty list to append discovered prime to
+    primes_list = []
+
+    # While loop
+    while number_range:
+            prime = number_range.pop()
+            primes_list.append(prime)
+            multiples = set(range(prime*2, n+1, prime))
+            number_range.difference_update(multiples)
+
+    # print our list of primes        
+    #print(primes_list)
+
+    # number of primes that were found
+    prime_count = len(primes_list)
+
+    # largest_prime 
+    largest_prime = max(primes_list)
+
+    # summary
+    print(f"There are {prime_count} prime numbers between 2 and {n}, the largest of which is {largest_prime} ")
+    
+    return primes_list
+
+
+primes_list = primes_founder(100)
+print(primes_list)
+
+
+primes_list = primes_founder(10000000)
+print(primes_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
