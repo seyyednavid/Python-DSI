fa794a47deb7815c2ab54d2b3abccfd243ef38c3
Navid Hejazi <navid.hejazi@Tensator.com>
2025-01-29 09:09:21 +0000

    Work on functions in python

---
+++ 013_Functions.py
@@ -0,0 +1,42 @@
+
+
+def happy_birthday():
+    pass
+
+happy_birthday()
+
+
+
+def happy_birthday():
+    print("Happy Birthday!")
+
+happy_birthday()
+
+
+
+def happy_birthday(name):
+    print(f"Happy Birthday {name}!")
+
+happy_birthday("Sue")
+happy_birthday("Sachin")
+
+
+
+def happy_birthday(name):
+    print(f"Happy Birthday {name}!")
+
+happy_birthday("Sue")
+happy_birthday("Sachin")
+happy_birthday()
+
+
+
+
+def happy_birthday(name= "Human"):
+    print(f"Happy Birthday {name}!")
+
+happy_birthday("Sue")
+happy_birthday("Sachin")
+happy_birthday()
+
+
