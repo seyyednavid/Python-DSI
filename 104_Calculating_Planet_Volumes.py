b42a1ac4c4ce753785d85ea620f00fa99e948479
Navid Hejazi <navid.hejazi@Tensator.com>
2025-01-31 09:32:41 +0000

    Calculating_Planet_Volumes with numpy arrays

---
+++ 104_Calculating_Planet_Volumes.py
@@ -0,0 +1,23 @@
+# The reason that numpy is so fast, because it uses only single data type 
+
+import numpy as np
+
+radii = np.array([2439.7, 6051.8, 6371, 3389.7, 69911, 58232, 25362, 24622])
+
+
+r = 10
+volume = 4/3 * np.pi * r ** 3
+print(volume)
+
+
+
+volumes = 4/3 * np.pi * radii ** 3
+print(volumes)
+
+
+
+radii = np.random.randint(1, 1000, 1000000)
+volumes = 4/3 * np.pi * radii ** 3
+print(volumes)
+
+
